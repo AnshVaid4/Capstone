af0780c285152a006029a9e2b804b3a547e76701
Ansh Vaid <anshvaid4@gmail.com>
2022-01-11 22:21:42 +0530

    done

--- Sniff code/V2/sniff.py
+++ Sniff code/V2/sniff.py
@@ -21,8 +21,31 @@ def process_packet(packet):
             print("TCP: ",packet.summary())
     except:
         print("UDP: ",packet.summary())
-    
-        
+
+    #counter=0      #=======================same as show()
+    #while True:
+    #    if packet.getlayer(counter) == None:
+            #break
+        #layer=packet.payload
+        #print("Layer: ",layer)
+        #print(packet.getlayer(counter))
+        #counter+=1
+    layers=packet.layers()              #print the layer names
+    layer=[]
+    for i in layers:
+        exp=str(i).replace("'","").replace("<","").replace(">","").replace(" ","")
+        try:                #for layer "RAW"
+            layer.append(exp.split(".")[3])
+        except:
+            pass
+    print(layer)
+    print(packet.show(dump=True))       #print layer with values
+    #print("\n\n\nLayers: \n",packet.payload.layers())
+    if packet[IP] != None:
+        print(packet[IP].version,packet[IP].src)
+    print("==============================================")        
+    #Layers:  [<class 'scapy.layers.inet.IP'>, <class 'scapy.layers.inet.TCP'>, <class 'scapy.packet.Raw'>]
+    #[<class 'scapy.layers.inet.IP'>, <class 'scapy.layers.inet.UDP'>, <class 'scapy.packet.Raw'>]    
 capture=sniff(prn=process_packet, store=False)
 
 
