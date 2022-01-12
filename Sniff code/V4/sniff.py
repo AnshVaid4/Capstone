f35153fdd2960b006e4590a66005a80ce192b1c8
Ansh Vaid <anshvaid4@gmail.com>
2022-01-13 01:03:27 +0530

    done

---
+++ Sniff code/V4/sniff.py
@@ -0,0 +1,102 @@
+from scapy.all import *
+from ipaddress import *
+from datetime import datetime
+import base64
+import os
+
+monsrcip=input("Enter the IP you want to monitor: ")
+monsrcport=input("Enter the source port you want to monitor: ")
+monhostport=input("Enter the port of your device you want to monitor: ")
+
+if monsrcip == "\n" or monsrcport == None or monhostport == None:
+    print("[-] All the input fields are necessary")
+    quit()
+
+
+logpacket="n"
+
+srcip=None
+destip=None
+srcport=None
+destport=None
+
+packetlen=None
+packetttl=None
+protocol=None
+
+flags=None
+
+date=None
+time=None
+
+b64enc=None
+
+
+ipobj = IPv4Network(monsrcip)
+dt=datetime.now()
+
+def process_packet(packet):
+
+    print("[+]    ",packet.summary())
+    
+    if packet.haslayer(IP) and packet[IP] != None:  #IP is scapy packet obj #name 'ip' is not defined
+        srcip=packet[IP].src
+        destip=packet[IP].dst                       #protocol=packet[IP].proto
+        packetlen=packet[IP].len
+        packetttl=packet[IP].ttl
+        if monsrcip != "N" and ((IPv4Address(srcip) in ipobj) or (IPv4Address(dstip) in ipobj)):
+            print("[-]Logged| Source IP: ",srcip," Destination IP: ",destip)
+            logpacket="y"
+
+        
+    if packet.haslayer(ARP) and packet[ARP] != None:
+       srcip=packet[ARP].psrc
+       destip=packet[ARP].pdst
+       if monsrcip != "N" and ((IPv4Address(srcip) in ipobj) or (IPv4Address(dstip) in ipobj)):
+            print("[-]Logged| Source IP: ",srcip," Destination IP: ",destip)
+            logpacket="y"
+       
+    
+    if packet.haslayer(TCP) and packet[TCP] != None:
+        srcport=packet[TCP].sport
+        destport=packet[TCP].dport
+        protocol="TCP"
+        flags=packet[TCP].flags
+        if monsrcport != "N" and monsrcport == srcport:
+            print("[-]Logged| Source port: ",srcport," Destination port: ",destport)
+            logpacket="y"
+        if monhostport != "N" and monhostport == destport:
+            print("[-]Logged| Destination port: ",destport," Source port: ",srcport)
+            logpacket="y"
+                                                                    #returns 443 if https
+    if packet.haslayer(UDP) and packet[UDP] != None:                                          #IndexError: Layer [UDP] not found
+        srcport=packet[UDP].sport
+        destport=packet[UDP].dport
+        protocol="UDP"
+        if monsrcport != "N" and monsrcport == srcport:
+            print("[-]Logged| Source port: ",srcport," Destination port: ",destport)
+            logpacket="y"
+        if monhostport != "N" and monhostport == destport:
+            print("[-]Logged| Destination port: ",destport," Source port: ",srcport)
+            logpacket="y"
+
+
+
+    if logpacket == "y":
+        datetime=dt.strftime("%d/%m/%Y %H:%M:%S")
+        datetime=datetime.split(" ")
+        date=datetime[0]
+        time=datetime[1]
+        
+        dump=hexdump(packet)
+        dump=dump.encode("ascii")
+        b64enc=base64.b64encode(dump)
+
+
+        logpacket == "n"
+    print("===========================================================================")        
+     
+capture=sniff(prn=process_packet, store=False)
+
+
+
