b9c837cfa3e62253b4dfdcce01968123965d7145
Ansh Vaid <anshvaid4@gmail.com>
2022-01-13 01:41:14 +0530

    done

--- Sniff code/V4/sniff.py
+++ Sniff code/V4/sniff.py
@@ -8,43 +8,47 @@ monsrcip=input("Enter the IP you want to monitor: ")
 monsrcport=input("Enter the source port you want to monitor: ")
 monhostport=input("Enter the port of your device you want to monitor: ")
 
-if monsrcip == "\n" or monsrcport == None or monhostport == None:
-    print("[-] All the input fields are necessary")
+if monsrcip == "":
+    print("[-] IP address cannot be blank")
     quit()
-
+    
+if monsrcport == "":
+    monsrcport="N"
+    
+if monhostport == "":
+    monhostport="N"
 
 logpacket="n"
 
-srcip=None
-destip=None
-srcport=None
-destport=None
-
-packetlen=None
-packetttl=None
-protocol=None
-
-flags=None
-
-date=None
-time=None
-
-b64enc=None
-
-
-ipobj = IPv4Network(monsrcip)
-dt=datetime.now()
+if monsrcip != "N":
+    ipobj = IPv4Network(monsrcip)
+    dt=datetime.now()
 
 def process_packet(packet):
+    global logpacket
+    srcip=None
+    destip=None
+    srcport=None
+    destport=None
 
-    print("[+]    ",packet.summary())
+    packetlen=None
+    packetttl=None
+    protocol=None
+
+    flags=None
+
+    date=None
+    time=None
+
+    b64enc=None
+    print("[+]",packet.summary())
     
     if packet.haslayer(IP) and packet[IP] != None:  #IP is scapy packet obj #name 'ip' is not defined
         srcip=packet[IP].src
         destip=packet[IP].dst                       #protocol=packet[IP].proto
         packetlen=packet[IP].len
         packetttl=packet[IP].ttl
-        if monsrcip != "N" and ((IPv4Address(srcip) in ipobj) or (IPv4Address(dstip) in ipobj)):
+        if monsrcip != "N" and ((IPv4Address(srcip) in ipobj) or (IPv4Address(destip) in ipobj)):
             print("[-]Logged| Source IP: ",srcip," Destination IP: ",destip)
             logpacket="y"
 
@@ -52,7 +56,7 @@ def process_packet(packet):
     if packet.haslayer(ARP) and packet[ARP] != None:
        srcip=packet[ARP].psrc
        destip=packet[ARP].pdst
-       if monsrcip != "N" and ((IPv4Address(srcip) in ipobj) or (IPv4Address(dstip) in ipobj)):
+       if monsrcip != "N" and ((IPv4Address(srcip) in ipobj) or (IPv4Address(destip) in ipobj)):
             print("[-]Logged| Source IP: ",srcip," Destination IP: ",destip)
             logpacket="y"
        
@@ -88,12 +92,13 @@ def process_packet(packet):
         date=datetime[0]
         time=datetime[1]
         
-        dump=hexdump(packet)
+        dump=str(hexdump(packet))
         dump=dump.encode("ascii")
         b64enc=base64.b64encode(dump)
 
-
-        logpacket == "n"
+        print("Source IP: ",srcip," Destination IP: ",destip,"\nSource port: ",srcport," Destination port: ",destport,"\nPacket length: ",packetlen," Packet TTL ",packetttl,
+              "\nProtocol: ",protocol," Flags ",flags,"\nDate: ",date," Time: ",time,"\n\nBase64 encoded RAW\n",b64enc)
+        logpacket="n"
     print("===========================================================================")        
      
 capture=sniff(prn=process_packet, store=False)
